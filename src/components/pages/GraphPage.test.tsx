6ee399d99993c7ba261dc316761620082f53008e
cyberpoetry17 <sweetbee0071@gmail.com>
2025-04-29 10:28:14 +0200

    Update: defaultGraph mock

--- src/components/pages/GraphPage.test.tsx
+++ src/components/pages/GraphPage.test.tsx
@@ -3,7 +3,7 @@ import GraphPage from "./GraphPage";
 import { FormProvider } from "../../store";
 
 
-const mockedActionGraph = {
+const defaultGraph = {
   "$schema": "test",
   "id": "test",
   "tenant_id": "1",
@@ -39,11 +39,10 @@ const mockedActionGraph = {
 
   ],
   "edges": [
-    
   ],
   "forms": [
     {
-      "id": "f_01jk7ap2r3ewf9gx6a9r09gzjv",
+      "id": "test",
       "name": "test form",
       "description": "test",
       "is_reusable": false,
@@ -51,47 +50,47 @@ const mockedActionGraph = {
         "type": "object",
         "properties": {
           "button": {
-            "avantos_type": "button",
+            "test_type": "button",
             "title": "Button",
             "type": "object"
           },
-          "dynamic_checkbox_group": {
-            "avantos_type": "checkbox-group",
+          "dynamic_test_type": {
+            "some_type": "test-group",
             "items": {
               "enum": [
-                "foo",
-                "bar",
-                "foobar"
+                "test1",
+                "test2",
+                "test3"
               ],
               "type": "string"
             },
             "type": "array",
             "uniqueItems": true
           },
-          "dynamic_object": {
-            "avantos_type": "object-enum",
+          "test_oject": {
+            "some_type": "object-enum",
             "enum": null,
-            "title": "Dynamic Object",
+            "title": "test object",
             "type": "object"
           },
           "email": {
-            "avantos_type": "short-text",
+            "some_type": "short-text",
             "format": "email",
             "title": "Email",
             "type": "string"
           },
           "id": {
-            "avantos_type": "short-text",
+            "some_type": "short-text",
             "title": "ID",
             "type": "string"
           },
           "multi_select": {
-            "avantos_type": "multi-select",
+            "some_type": "multi-select",
             "items": {
               "enum": [
-                "foo",
-                "bar",
-                "foobar"
+                "test",
+                "test",
+                "tes"
               ],
               "type": "string"
             },
@@ -99,12 +98,12 @@ const mockedActionGraph = {
             "uniqueItems": true
           },
           "name": {
-            "avantos_type": "short-text",
+            "some_type": "short-text",
             "title": "Name",
             "type": "string"
           },
           "notes": {
-            "avantos_type": "multi-line-text",
+            "some_type": "multi-line-text",
             "title": "Notes",
             "type": "string"
           }
@@ -116,34 +115,34 @@ const mockedActionGraph = {
         ]
       ,
         "button": {
-          "selector_field": "title",
-          "payload_fields": {
-            "userId": {
+          "test_field": "title",
+          "test_payload": {
+            "testId": {
               "type": "form_field",
               "value": "id"
             }
           },
-          "endpoint_id": "te_01jk7ap2r0ewfbrfd53sx46hd2"
+          "test_endpoint": "test1"
         },
-        "dynamic_checkbox_group": {
-          "selector_field": "title",
-          "payload_fields": {
+        "test_checkbox": {
+          "test_selector": "title",
+          "test_payload": {
             "userId": {
               "type": "form_field",
               "value": "id"
             }
           },
-          "endpoint_id": "te_01jk7ap2r0ewfbrfd53sx46hd2"
+          "test_endpoint": "test2"
         },
-        "dynamic_object": {
-          "selector_field": "title",
-          "payload_fields": {
+        "test_object": {
+          "test_selector": "title",
+          "test_payload": {
             "userId": {
               "type": "form_field",
               "value": "id"
             }
           },
-          "endpoint_id": "te_01jk7ap2r0ewfbrfd53sx46hd2"
+          "test_endpoint": "test3"
         }
       }
     },
@@ -155,7 +154,7 @@ const mockedActionGraph = {
 
 global.fetch = jest.fn(() =>
   Promise.resolve({
-    json: () => Promise.resolve(mockedActionGraph),
+    json: () => Promise.resolve(defaultGraph),
   })
 ) as jest.Mock;
 
